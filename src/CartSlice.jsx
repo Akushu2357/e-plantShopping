9661a5436adf65cbea6af8f602d9686f55fe2e2e
Akushu <tang.tang2004@gmail.com>
2024-09-28 17:01:04 +0700

    update

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,22 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const { name, image, cost } = action.payload
+      const findItem = state.items.find(item => item.name === name)
+      if (findItem) {
+        findItem.quantity += 1;
+      } else {
+        state.items.push({name, image, cost, quantity: 1});
+      }
     },
     removeItem: (state, action) => {
+      state.items = state.items.filter(item => action.payload.name !== item.name);
     },
     updateQuantity: (state, action) => {
-
-    
+      const updateItem = state.items.find(item => item.name === action.payload.name);
+      if (updateItem) {
+        updateItem.quantity = action.payload.quantity;
+      }
     },
   },
 });
--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/shoppingreact",
   plugins: [react()],
-})
+  optimizeDeps: {
+    include: ['react', 'react-dom'],
+  }
+});
